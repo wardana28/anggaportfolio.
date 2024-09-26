a3b1a53251d602ee4606353c9b6c49c70626c7ae
wardana28 <114861399+wardana28@users.noreply.github.com>
2024-09-26 22:01:44 +0700

    menambah tahun dengan JavaScript

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const navUL = document.querySelector("nav ul");
 menu.addEventListener("click", function () {
   navUL.classList.toggle("slide");
 });
+
+// footer
+document.getElementById("year").textContent = new Date().getFullYear();
