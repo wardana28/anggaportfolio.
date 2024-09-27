31fa41abe5b6f929b267d5fbb9ba1356f9c13fbc
wardana28 <114861399+wardana28@users.noreply.github.com>
2024-09-27 11:22:50 +0700

    mengubah animasi nama

--- script.js
+++ script.js
@@ -5,5 +5,27 @@ menu.addEventListener("click", function () {
   navUL.classList.toggle("slide");
 });
 
+// animasi ketik
+var typed = new Typed("#text", {
+  strings: ["Hi my name is "],
+  typeSpeed: 50,
+  cursorChar: "",
+});
+
+var typed = new Typed("#text-1", {
+  strings: [" Angga", " tri", " Wardana"],
+  typeSpeed: 50,
+  cursorChar: "",
+  startDelay: 1000,
+  loop: true,
+});
+
+var typed = new Typed("#text-2", {
+  strings: ["Frontend Developer"],
+  typeSpeed: 50,
+  cursorChar: "",
+  startDelay: 1500,
+});
+
 // footer
 document.getElementById("year").textContent = new Date().getFullYear();
